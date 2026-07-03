e7e070ed5c2ad00b6376e5a7ab32b99864352e5c
Sysfaz <sysfaz@thepwners.org>
2026-07-04 01:52:12 +0800

    Updated to autostart hyprpolkitagent

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -34,6 +34,7 @@ local code         = "subl"
 hl.on("hyprland.start", function () 
     hl.exec_cmd("waybar")
     hl.exec_cmd("awww-daemon")
+    hl.exec_cmd("/usr/lib/hyprpolkitagent/hyprpolkitagent")
     hl.exec_cmd("wl-paste --type text --watch cliphist store")
     hl.exec_cmd("wl-paste --type image --watch cliphist store")
 end)
